4c172245dd7bb1a77dc175d7bb81e956da6af20c
Antoniowski <r97.antonio@gmail.com>
2023-05-18 23:42:42 +0200

    Modifiche Main Menu * Modificato il menu in modo che mostri titoli e scritte di colore   bianco quando il menu si trova di notte * Aggiunto PNG del logo bianco

--- Assets/Code/Scripts/Managers/Scene Managers/Menus/MainMenuManager.cs
+++ Assets/Code/Scripts/Managers/Scene Managers/Menus/MainMenuManager.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro;
 
 //Classe che gestisce la scena del menu principale
 public class MainMenuManager : MonoBehaviour
 {
     [Header("Lights")]
     [SerializeField] GameObject _lights;
+    [Space]
+    [Header("Title")]
+    [SerializeField] GameObject title;
+    [SerializeField] GameObject whiteTitle;
+    [Space]
+    [Header("Buttons")]
+    [SerializeField] GameObject playBtn;
+    [SerializeField] GameObject settingsBtn;
+    [SerializeField] GameObject quitBtn;
 
     
     
@@ -49,16 +59,20 @@ public class MainMenuManager : MonoBehaviour
             {
                 case var expression when(dateTimeHour >= 6 && dateTimeHour < 11):
                     xLightRotation = 180;
+                    SetTextToBlack();
                     break;
                 case var expression when(dateTimeHour >= 11 && dateTimeHour < 17):
                     xLightRotation = 79;
+                    SetTextToBlack();
                     break;
                 case var expression when(dateTimeHour >= 17 && dateTimeHour < 20):
                     print("enter");
                     xLightRotation = 0;
+                    SetTextToBlack();
                     break;
                 case var expression when(dateTimeHour >= 20 || dateTimeHour < 6):
                     xLightRotation = -50;
+                    SetTextToWhite();
                     break;
                 default:
                     break;
@@ -73,12 +87,15 @@ public class MainMenuManager : MonoBehaviour
                 {
                     case var expression when(dateTimeHour >= 6 && dateTimeHour < 11):
                         xLightRotation = 180;
+                        SetTextToBlack();
                         break;
                     case var expression when(dateTimeHour >= 11):
                         xLightRotation = 79;
+                        SetTextToBlack();
                         break;
                     case var expression when(dateTimeHour >= 0 && dateTimeHour < 6):
                         xLightRotation = -50;
+                        SetTextToWhite();
                         break;
                     default:
                         break;
@@ -90,12 +107,15 @@ public class MainMenuManager : MonoBehaviour
                 {
                     case var expression when(dateTimeHour >= 1 && dateTimeHour < 5):
                         xLightRotation = 79;
+                        SetTextToBlack();
                         break;
                     case var expression when(dateTimeHour >= 5 && dateTimeHour < 8):
                         xLightRotation = 0;
+                        SetTextToBlack();
                         break;
                     case var expression when(dateTimeHour >= 8):
                         xLightRotation = -50;
+                        SetTextToWhite();
                         break;
                     default:
                         break;
@@ -103,5 +123,28 @@ public class MainMenuManager : MonoBehaviour
             }
         }
         mainLight.transform.rotation = Quaternion.Euler(xLightRotation, yLightRotation, 0);
+    }
+
+    private void SetTextToWhite()
+    {
+        if(title.activeInHierarchy == false)
+            return;
+
+        title.SetActive(false);
+        whiteTitle.SetActive(true);
+        playBtn.GetComponentInChildren<TMP_Text>().color = Color.white;
+        settingsBtn.GetComponentInChildren<TMP_Text>().color = Color.white;
+        quitBtn.GetComponentInChildren<TMP_Text>().color = Color.white;
+    }
+    private void SetTextToBlack()
+    {
+        if(title.activeInHierarchy == true)
+            return;
+
+        title.SetActive(true);
+        whiteTitle.SetActive(false);
+        playBtn.GetComponent<TMP_Text>().color = new Color(75,75,75,1);
+        settingsBtn.GetComponent<TMP_Text>().color = new Color(75,75,75,1);
+        quitBtn.GetComponent<TMP_Text>().color = new Color(75,75,75,1);
     }
 }
